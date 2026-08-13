3c71df7c5124e15201e4a843216859ea7f05a8cd
nanda kishore <nandakishore24951a05l9@gmail.com>
2026-08-13 21:47:15 +0530

    render error

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,12 +1,7 @@
 <script setup lang="ts">
 import { portfolio } from "../services/portfolioStore";
 import Social from "./Social.vue";
-import Link from "./Link.vue";
-import Clickable from "./Clickable.vue";
-import LangSwitch from "./LangSwitch.vue";
 import NotchSection from "./NotchSection.vue";
-import { t } from "../i18n/utils/translate";
-import { locale } from "../i18n/store";
 import ButtonRound from "./ButtonRound.vue";
 import { lenis } from "../composables/useScroll";
 import ArrowRightLong from "./icons/ArrowRightLong.vue";
@@ -41,33 +36,6 @@ const { withSocial = true } = defineProps<Props>();
       </div>
       <div class="footer-top">
         <Social v-if="withSocial" />
-        <div class="footer-top-links">
-          <div class="footer-top-links-legal">
-            <Clickable renderAs="div">
-              <Link
-                :href="locale === 'de' ? '/de/privacy' : '/privacy'"
-                class="footer-link"
-                :external="true"
-                data-cursor="circle-white"
-                data-sound="click"
-                data-hoversound="hover"
-                >{{ t("privacy") }}</Link
-              >
-            </Clickable>
-            <Clickable renderAs="div">
-              <Link
-                :href="locale === 'de' ? '/de/legal' : '/legal'"
-                class="footer-link children-unclickable"
-                :external="true"
-                data-cursor="circle-white"
-                data-sound="click"
-                data-hoversound="hover"
-                >{{ t("legal") }}</Link
-              >
-            </Clickable>
-          </div>
-          <LangSwitch />
-        </div>
       </div>
       <div class="footer-credits">
         <p>© {{ new Date().getFullYear() }} {{ portfolio.profile.fullName }}</p>
